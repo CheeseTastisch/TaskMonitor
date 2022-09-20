5230932d291115ce7fd8346dc4f9128caa76a590
Lian <cheesetastisch@gmail.com>
2022-09-20 21:02:14 +0200

    ClientTest

--- Client/src/main/java/me/taskmonitor/client/ClientTest.java
+++ Client/src/main/java/me/taskmonitor/client/ClientTest.java
@@ -1,6 +1,7 @@
 package me.taskmonitor.client;
 
 import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
 import oshi.hardware.GraphicsCard;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.PhysicalMemory;
@@ -17,35 +18,68 @@ public class ClientTest {
         // Spezifikationen
 
         // Computername
-        String hostname = InetAddress.getLocalHost().getHostName();
-        hostname = hostname.endsWith(".local") ? hostname.replace(".local", "") : hostname;
-        System.out.println(hostname);
-
+//        String hostname = InetAddress.getLocalHost().getHostName();
+//        hostname = hostname.endsWith(".local") ? hostname.replace(".local", "") : hostname;
+//        System.out.println(hostname);
+//
         SystemInfo systemInfo = new SystemInfo();
 
 
 
         HardwareAbstractionLayer hal = systemInfo.getHardware();
 
-        System.out.println("----");
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getVendor());
         System.out.println(hal.getProcessor().getProcessorIdentifier().getName());
-        System.out.println(hal.getProcessor().getPhysicalProcessorCount());
-        System.out.println(hal.getProcessor().getLogicalProcessorCount());
-        System.out.println(hal.getProcessor().getMaxFreq());
-
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getFamily());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getModel());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getStepping());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getProcessorID());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getIdentifier());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().isCpu64bit());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getVendorFreq());
+        System.out.println(hal.getProcessor().getProcessorIdentifier().getMicroarchitecture());
         System.out.println("----");
-        PhysicalMemory firstMemory = hal.getMemory().getPhysicalMemory().get(0);
-        System.out.println(firstMemory.getClockSpeed());
-        System.out.println(firstMemory.getMemoryType());
-        System.out.println(hal.getMemory().getTotal());
+        System.out.println(hal.getProcessor().getMaxFreq());
+        System.out.println(Arrays.toString(hal.getProcessor().getCurrentFreq()));
 
-        for (GraphicsCard graphicsCard : hal.getGraphicsCards()) {
+        for (CentralProcessor.LogicalProcessor logicalProcessor : hal.getProcessor().getLogicalProcessors()) {
             System.out.println("----");
-            System.out.println(graphicsCard.getName());
-            System.out.println(graphicsCard.getVRam());
+            System.out.println(logicalProcessor.getProcessorNumber());
+            System.out.println(logicalProcessor.getPhysicalProcessorNumber());
+            System.out.println(logicalProcessor.getPhysicalPackageNumber());
+            System.out.println(logicalProcessor.getNumaNode());
+            System.out.println(logicalProcessor.getProcessorGroup());
         }
 
-        System.out.println(hal.getComputerSystem().getManufacturer());
+        for (CentralProcessor.PhysicalProcessor physicalProcessor : hal.getProcessor().getPhysicalProcessors()) {
+            System.out.println("----");
+            System.out.println(physicalProcessor.getPhysicalProcessorNumber());
+            System.out.println(physicalProcessor.getPhysicalPackageNumber());
+            System.out.println(physicalProcessor.getEfficiency());
+            System.out.println(physicalProcessor.getIdString());
+        }
+
+        System.out.println("----");
+        System.out.println(Arrays.toString(hal.getProcessor().getSystemCpuLoadTicks()));
+        System.out.println(Arrays.toString(hal.getProcessor().getSystemCpuLoadTicks()));
+        System.out.println(hal.getProcessor().getLogicalProcessorCount());
+        System.out.println(hal.getProcessor().getPhysicalProcessorCount());
+        System.out.println(hal.getProcessor().getContextSwitches());
+        System.out.println(hal.getProcessor().getInterrupts());
+
+
+//
+//        System.out.println("----");
+//        PhysicalMemory firstMemory = hal.getMemory().getPhysicalMemory().get(0);
+//        System.out.println(firstMemory.getClockSpeed());
+//        System.out.println(firstMemory.getMemoryType());
+//        System.out.println(hal.getMemory().getTotal());
+//
+//        for (GraphicsCard graphicsCard : hal.getGraphicsCards()) {
+//            System.out.println("----");
+//            System.out.println(graphicsCard.getName());
+//            System.out.println(graphicsCard.getVRam());
+//        }
 
         // Jeder Tick
     }
